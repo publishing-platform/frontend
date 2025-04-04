8cc441e555ea46850d890247e121d3469707d25d
neilha <neilharrold@gmail.com>
2025-04-04 16:34:46 +0100

    https://github.com/publishing-platform/signon/issues/25

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
 
   # Static error page routes - in practice used only during deploy, these don't have a
   # published route so can't be accessed from outside
   get "/static-error-pages/:error_code.html", to: "static_error_pages#show"
 
   get "*path" => "content_items#show"
+
+  get "up" => "rails/health#show", as: :rails_health_check
 end
